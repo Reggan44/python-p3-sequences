a8d31c9b2c2f18af31f7c9006263dd168c644436
Reggan Nzuki <wambuareggan@gmail.com>
2025-08-08 18:23:24 +0300

    sequences

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        print([])
+        return
+    elif length == 1:
+        print([0])
+        return
+    elif length == 2:
+        print([0, 1])
+        return
+    else:
+        fibs = [0, 1]
+        for i in range(2, length):
+            fibs.append(fibs[-1] + fibs[-2])
+        print(fibs)
